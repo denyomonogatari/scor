266f5dc216f2946eda04299d0c549b76e9f9c446
denyomonogatari <dvictor4@calstatela.edu>
2018-03-30 06:01:48 -0700

    Update for loop in MainPage and create SemesterPage to display the courses for a semester

--- src/main/SemesterPage.java
+++ src/main/SemesterPage.java
@@ -13,30 +13,29 @@ import javax.servlet.http.HttpSession;
 import models.Semester;
 import models.User;
 
-@WebServlet("/main/MainPage")
-public class MainPage extends HttpServlet {
+@WebServlet("/main/SemesterPage")
+public class SemesterPage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		// Get a reference to the session
 		HttpSession session = request.getSession();
-		
+
 		// Get a reference to the NoteUser object stored in the session
 		User user = (User) session.getAttribute("user");
-		
+
 		// If the User doesn't exist, then they didn't login. So, kick them back to Login
 		if (user == null) {
 			response.sendRedirect("../auth/Login");
 			return;
 		}
-		
+
 		// Set the content type to HTML
 		response.setContentType("text/html");
-		
+
 		// Get a Print Writer
 		PrintWriter out = response.getWriter();
-	
+
 		out.println("<!DOCTYPE html>");		
 		out.println("<html lang=\"en\">");
 		out.println("<head>");
@@ -44,7 +43,7 @@ public class MainPage extends HttpServlet {
 		out.println("        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
 		out.println("        <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">");
 		out.println("        <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">");
-		out.println("        <title>Main Page</title>");
+		out.println("        <title>Semester Page</title>");
 		out.println("</head>");
 		out.println("<body>");
 		out.println("<div class=\"container\">");
@@ -52,12 +51,14 @@ public class MainPage extends HttpServlet {
 		out.println("<h1 class=\"display-2\">Hello, " + user.getFirst() + "</h1>");
 		out.println("<div class=\"row\">");
 		out.println("	<div class=\"col-6\">");
-		out.println("		<h2>Semesters</h2>");
+		out.println("		<h2>Courses</h2>");
 		out.println("		<ul>");
-		for (int i = 0; i < user.getSemesters().size(); i++)
-			out.println("			<li><a href=\"SemesterPage?id=" + i + "\">" + user.getSemesters().get(i).getSeason() + " " + user.getSemesters().get(i).getYear() + " " + String.format("%.2f", user.getSemesters().get(i).getGPA()) + "</a></li>");
-		out.println("		</ul>");
+		int id = Integer.parseInt(request.getParameter("id"));
 		
+		for (int i = 0; i < user.getSemesters().get(id).getCourses().size(); i++) 
+			out.println("			<li><a href=#>" + user.getSemesters().get(id).getCourses().get(i).getName() + " " + user.getSemesters().get(id).getCourses().get(i).getGrade() + "</a></li>");
+		out.println("		</ul>");
+
 		out.println("	</div>");
 		out.println("	<div class=\"col-6\">");
 		out.println("		<h2>Dates</h2>");
@@ -66,7 +67,7 @@ public class MainPage extends HttpServlet {
 		out.println("	<div class=\"col\">");
 		out.println("	</div>");
 		out.println("</div>");
-		
+
 		out.println("</div>");
 		out.println("</body>");
 		out.println("</html>");
