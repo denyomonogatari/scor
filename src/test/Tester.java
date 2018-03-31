9f6d91d043f892aeb1e337add1ca53916045b689
arodr856 <arodr856@gmail.com>
2018-03-31 10:18:53 -0700

    FlashCard model implementation

--- src/test/Tester.java
+++ src/test/Tester.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import models.Assessment;
 import models.Assignment;
+import models.AssignmentType;
 import models.Course;
 import models.GradingScale;
 import models.Semester;
@@ -16,27 +17,32 @@ public class Tester {
 		long currentMilliseconds = new Date().getTime();
 
 		Assignment lab1 = new Assignment("Hello World Program", 95, 100, true, false, new Date(currentMilliseconds),
-				new Date(currentMilliseconds + 100_000_000));
+				new Date(currentMilliseconds + 100_000_000), AssignmentType.LAB);
 		// System.out.println(lab1);
 
 		Assignment lab2 = new Assignment("Strings Lab", 80, 90, true, false,
-				new Date(currentMilliseconds + 100_000_000), new Date(currentMilliseconds + 900_000_000));
+				new Date(currentMilliseconds + 100_000_000), new Date(currentMilliseconds + 900_000_000),
+				AssignmentType.LAB);
 		// System.out.println(lab2);
 
-		Assessment labs = new Assessment("Labs", 20);
+		Assessment labs = new Assessment(AssignmentType.LAB, 20);
 		labs.addAssignment(lab1);
 		labs.addAssignment(lab2);
 		// System.out.println(labs);
 
-		GradingScale gradingScale1 = new GradingScale(95, 90, 87, 85, 80, 77, 75); // ABC/NC grading scale
+		GradingScale gradingScale1 = new GradingScale(95, 90, 87, 85, 80, 77, 75); //
+		// ABC/NC grading scale
 		// System.out.println(gradingScale2);
 
-		Assessment quizzes = new Assessment("Midterms", 50);
-		quizzes.addAssignment(new Assignment("Midterm 1", 75, 100, true, false, new Date(currentMilliseconds), null));
-		quizzes.addAssignment(new Assignment("Midterm 2", 70, 100, true, false, new Date(currentMilliseconds), null));
+		Assessment quizzes = new Assessment(AssignmentType.QUIZ, 50);
+		quizzes.addAssignment(new Assignment("Midterm 1", 75, 100, true, false, new Date(currentMilliseconds), null,
+				AssignmentType.QUIZ));
+		quizzes.addAssignment(new Assignment("Midterm 2", 70, 100, true, false, new Date(currentMilliseconds), null,
+				AssignmentType.QUIZ));
 
-		Assessment finalTest = new Assessment("Final", 30);
-		finalTest.addAssignment(new Assignment("Final", 95, 100, true, false, new Date(currentMilliseconds), null));
+		Assessment finalTest = new Assessment(AssignmentType.FINAL, 30);
+		finalTest.addAssignment(new Assignment("Final", 95, 100, true, false, new Date(currentMilliseconds), null,
+				AssignmentType.FINAL));
 
 		Course cs2011 = new Course("CS 2011", 3, gradingScale1);
 		cs2011.addAssessment(labs);
@@ -51,9 +57,9 @@ public class Tester {
 
 		Course cs3220 = new Course("CS 3220", 3, gradingScale2);
 
-		Assessment homework = new Assessment("HW", 100);
-		homework.addAssignment(
-				new Assignment("HW1: Create Notes", 95, 100, true, false, new Date(currentMilliseconds), null));
+		Assessment homework = new Assessment(AssignmentType.HOMEWORK, 100);
+		homework.addAssignment(new Assignment("HW1: Create Notes", 95, 100, true, false, new Date(currentMilliseconds),
+				null, AssignmentType.HOMEWORK));
 		cs3220.addAssessment(homework);
 
 		Semester spring2018 = new Semester(Season.SPRING, 2018);
@@ -66,26 +72,27 @@ public class Tester {
 		// System.out.println(spring2018);
 		System.out.println(johnDoe);
 
-		GradingScale gradingScale1 = new GradingScale(92, 90, 87, 83, 80, 75, 70);
+		// GradingScale gradingScale1 = new GradingScale(92, 90, 87, 83, 80, 75, 70);
 
-		User johnDoe = new User("John", "Doe", "john@doe.com", "abcd");
+		// User johnDoe = new User("John", "Doe", "john@doe.com", "abcd");
 
-		Semester spring2018 = new Semester(Season.SPRING, 2018);
+		// Semester spring2018 = new Semester(Season.SPRING, 2018);
 
 		Course cs3035 = new Course("CS 3035", 3, gradingScale1);
 
-		Assessment assessment1 = new Assessment("Labs and quizzes", 30);
-		Assessment assessment2 = new Assessment("Midterm", 20);
-		Assessment assessment3 = new Assessment("Homework", 30);
-		Assessment assessment4 = new Assessment("Final", 20);
+		Assessment assessment1 = new Assessment(AssignmentType.LABSANDQUIZZES, 30);
+		Assessment assessment2 = new Assessment(AssignmentType.MIDTERM, 20);
+		Assessment assessment3 = new Assessment(AssignmentType.HOMEWORK, 30);
+		Assessment assessment4 = new Assessment(AssignmentType.FINAL, 20);
 
-		Assignment assignment1 = new Assignment("Lab1", 80, 100, true, false, null, null);
-		Assignment assignment2 = new Assignment("Lab2", 10, 13, true, false, null, null);
-		Assignment assignment3 = new Assignment("Lab3", 11, 13, true, false, null, null);
-		Assignment assignment4 = new Assignment("Midterm", 30, 37, true, false, null, null);
-		Assignment assignment5 = new Assignment("Homework 1", 9, 10, true, false, null, null);
-		Assignment assignment6 = new Assignment("Homework 2", 8, 10, true, false, null, null);
-		Assignment assignment7 = new Assignment("Final", 83, 100, true, false, null, null);
+		Assignment assignment1 = new Assignment("Lab1", 80, 100, true, false, null, null,
+				AssignmentType.LABSANDQUIZZES);
+		Assignment assignment2 = new Assignment("Lab2", 10, 13, true, false, null, null, AssignmentType.LABSANDQUIZZES);
+		Assignment assignment3 = new Assignment("Lab3", 11, 13, true, false, null, null, AssignmentType.LABSANDQUIZZES);
+		Assignment assignment4 = new Assignment("Midterm", 30, 37, true, false, null, null, AssignmentType.MIDTERM);
+		Assignment assignment5 = new Assignment("Homework 1", 9, 10, true, false, null, null, AssignmentType.HOMEWORK);
+		Assignment assignment6 = new Assignment("Homework 2", 8, 10, true, false, null, null, AssignmentType.HOMEWORK);
+		Assignment assignment7 = new Assignment("Final", 83, 100, true, false, null, null, AssignmentType.FINAL);
 
 		assessment1.addAssignment(assignment1);
 		assessment1.addAssignment(assignment2);
@@ -102,33 +109,41 @@ public class Tester {
 
 		spring2018.addCourse(cs3035);
 
-		Course cs3220 = new Course("CS 3220", 3, gradingScale1);
+		// Course cs3220 = new Course("CS 3220", 3, gradingScale1);
 
-		Assessment assessment5 = new Assessment("Homework and Lab Assignments", 20);
-		Assessment assessment6 = new Assessment("Quizzes", 25);
-		Assessment assessment7 = new Assessment("Midterm Exams", 25);
-		Assessment assessment8 = new Assessment("Final Exam", 25);
-		Assessment assessment9 = new Assessment("Attendence", 5);
+		Assessment assessment5 = new Assessment(AssignmentType.HOMEWORKANDLABS, 20);
+		Assessment assessment6 = new Assessment(AssignmentType.QUIZ, 25);
+		Assessment assessment7 = new Assessment(AssignmentType.MIDTERM, 25);
+		Assessment assessment8 = new Assessment(AssignmentType.FINAL, 25);
+		Assessment assessment9 = new Assessment(AssignmentType.ATTENDANCE, 5);
 
-		Assignment assignment8 = new Assignment("Homework 1 Two Paragraphs", 100, 100, true, false, null, null);
-		Assignment assignment9 = new Assignment("Homework 2 Notes", 90, 100, true, false, null, null);
-		Assignment assignment10 = new Assignment("Lab 1 Deployement", 100, 100, true, false, null, null);
-		Assignment assignment11 = new Assignment("Lab 2 Request Summary", 80, 100, true, false, null, null);
-		Assignment assignment12 = new Assignment("Lab 3 Export to Excel", 90, 100, true, false, null, null);
+		Assignment assignment8 = new Assignment("Homework 1 Two Paragraphs", 100, 100, true, false, null, null,
+				AssignmentType.HOMEWORKANDLABS);
+		Assignment assignment9 = new Assignment("Homework 2 Notes", 90, 100, true, false, null, null,
+				AssignmentType.HOMEWORKANDLABS);
+		Assignment assignment10 = new Assignment("Lab 1 Deployement", 100, 100, true, false, null, null,
+				AssignmentType.HOMEWORKANDLABS);
+		Assignment assignment11 = new Assignment("Lab 2 Request Summary", 80, 100, true, false, null, null,
+				AssignmentType.HOMEWORKANDLABS);
+		Assignment assignment12 = new Assignment("Lab 3 Export to Excel", 90, 100, true, false, null, null,
+				AssignmentType.HOMEWORKANDLABS);
 
-		Assignment assignment13 = new Assignment("HTML, CSS, and Deployment", 8, 10, true, false, null, null);
-		Assignment assignment14 = new Assignment("HTTP Requests", 9, 10, true, false, null, null);
-		Assignment assignment15 = new Assignment("HTTP Responses", 8, 10, true, false, null, null);
-		Assignment assignment16 = new Assignment("Cookies  and Session", 9, 10, true, false, null, null);
+		Assignment assignment13 = new Assignment("HTML, CSS, and Deployment", 8, 10, true, false, null, null,
+				AssignmentType.QUIZ);
+		Assignment assignment14 = new Assignment("HTTP Requests", 9, 10, true, false, null, null, AssignmentType.QUIZ);
+		Assignment assignment15 = new Assignment("HTTP Responses", 8, 10, true, false, null, null, AssignmentType.QUIZ);
+		Assignment assignment16 = new Assignment("Cookies  and Session", 9, 10, true, false, null, null,
+				AssignmentType.QUIZ);
 		Assignment assignment17 = new Assignment("JSP Scripting Elements and Directives", 10, 10, true, false, null,
-				null);
+				null, AssignmentType.QUIZ);
 
-		Assignment assignment18 = new Assignment("Midterm Exam", 95, 100, true, false, null, null);
+		Assignment assignment18 = new Assignment("Midterm Exam", 95, 100, true, false, null, null,
+				AssignmentType.MIDTERM);
 
-		Assignment assignment19 = new Assignment("Final", 85, 100, true, false, null, null);
+		Assignment assignment19 = new Assignment("Final", 85, 100, true, false, null, null, AssignmentType.FINAL);
 
-		Assignment assignment20 = new Assignment("01-22-2018", 1, 1, true, false, null, null);
-
+		Assignment assignment20 = new Assignment("01-22-2018", 1, 1, true, false, null, null,
+				AssignmentType.HOMEWORKANDLABS);
 		assessment5.addAssignment(assignment8);
 		assessment5.addAssignment(assignment9);
 		assessment5.addAssignment(assignment10);
