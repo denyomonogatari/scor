1c5b5790d99abb1b49635d4cb0cc092352d82c9d
denyomonogatari <dvictor4@calstatela.edu>
2018-04-04 15:01:03 -0700

    CoursePage refactor

--- src/main/CoursePage.java
+++ src/main/CoursePage.java
@@ -2,6 +2,7 @@ package main;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +11,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import models.Assessment;
+import models.Assignment;
+import models.Course;
 import models.User;
 
 @WebServlet("/main/CoursePage")
@@ -45,51 +49,56 @@ public class CoursePage extends HttpServlet {
 		out.println("        <title>Course Page</title>");
 		out.println("</head>");
 		out.println("<body>");
-		out.println("<div class=\"container\">");
-		out.println("<p class=\"text-right\"><a href=\"../auth/Logout\">Logout</a></p>");
-		out.println("<h1 class=\"display-2\">Hello, " + user.getFirst() + "</h1>");
-		out.println("<div class=\"row\">");
-		out.println("	<div class=\"col-12\">");
+		out.println("	<div class=\"container\">");
+		out.println("		<p class=\"text-right\"><a href=\"../auth/Logout\">Logout</a></p>");
+		out.println("		<h1 class=\"display-2\">Hello, " + user.getFirst() + "</h1>");
+		out.println("		<div class=\"row\">");
+		out.println("			<div class=\"col-12\">");
 		
 		int semesterId = Integer.parseInt(request.getParameter("semesterId"));
 		int courseId = Integer.parseInt(request.getParameter("courseId"));
 		
-		out.println("		<h2>" + user.getSemesters().get(semesterId).getCourses().get(courseId).getName() + "</h2>");
-		out.println("		<h2>Percent: <strong>" + String.format("%.2f", user.getSemesters().get(semesterId).getCourses().get(courseId).getPercentEarned()) + "%</strong></h2>");
-		out.println("		<h2>Grade: <strong>" + user.getSemesters().get(semesterId).getCourses().get(courseId).getGrade() + "</strong></h2>");
-
-		for (int i = 0; i < user.getSemesters().get(semesterId).getCourses().get(courseId).getAssessments().size(); i++) {
-			out.println("<h3>" + user.getSemesters().get(semesterId).getCourses().get(courseId).getAssessments().get(i).getType() + " " + String.format("%.2f %%", user.getSemesters().get(semesterId).getCourses().get(courseId).getAssessments().get(i).getScore()) + " out of " + user.getSemesters().get(semesterId).getCourses().get(courseId).getAssessments().get(i).getWorth() + "%</h3>");
-			out.println("	<table class=\"table\">");
-			out.println("<tr>");
-			out.println("<th>Name</th>");
-			out.println("<th>Score</th>");
-			out.println("<th>Worth</th>");
-			out.println("<th>Options</th>");
-			out.println("</tr>");
-			for (int j = 0; j <  user.getSemesters().get(semesterId).getCourses().get(courseId).getAssessments().get(i).getAssignments().size(); j++) {
-				out.println("	<tr>");
-				out.println("		<td>" + user.getSemesters().get(semesterId).getCourses().get(courseId).getAssessments().get(i).getAssignments().get(j).getName() + "</td>");
-				out.println("		<td>" + user.getSemesters().get(semesterId).getCourses().get(courseId).getAssessments().get(i).getAssignments().get(j).getScore() + "</td>");
-				out.println("		<td>" + user.getSemesters().get(semesterId).getCourses().get(courseId).getAssessments().get(i).getAssignments().get(j).getTotal() + "</td>");
-				out.println("		<td>");
-				out.println("			<a class=\"btn btn-primary btn-secondary\" href=\"#\" role=\"button\">Edit</a> <a class=\"btn btn-primary btn-danger\" href=\"#\" role=\"button\">Delete</a>");
-				out.println("		</td>");
-				out.println("	</tr>");
+		Course course = user.getSemesters().get(semesterId).getCourses().get(courseId); // get course
+		
+		out.println("				<h2>" + course.getName() + "</h2>");
+		out.println("				<h2>Percent: <strong>" + String.format("%.2f", course.getPercentEarned()) + "%</strong></h2>");
+		out.println("				<h2>Grade: <strong>" + course.getGrade() + "</strong></h2>");
+		
+		ArrayList<Assessment> assessments = course.getAssessments();
+		for (int i = 0; i < assessments.size(); i++) { // get assessments for course to display them
+			out.println("				<h3>" + assessments.get(i).getType() + " " + String.format("%.2f %%", assessments.get(i).getScore()) + " out of " + assessments.get(i).getWorth() + "%</h3>");
+			out.println("				<table class=\"table\">");
+			out.println("					<tr>");
+			out.println("						<th>Name</th>");
+			out.println("						<th>Score</th>");
+			out.println("						<th>Worth</th>");
+			out.println("						<th>Options</th>");
+			out.println("					</tr>");
+			
+			ArrayList<Assignment> assignments = assessments.get(i).getAssignments(); // get assignments for assessment i
+			for (int j = 0; j <  assignments.size(); j++) { // iterate through assignments to display them
+				out.println("					<tr>");
+				out.println("						<td>" + assignments.get(j).getName() + "</td>");
+				out.println("						<td>" + assignments.get(j).getScore() + "</td>");
+				out.println("						<td>" + assignments.get(j).getTotal() + "</td>");
+				out.println("						<td>");
+				out.println("							<a class=\"btn btn-primary btn-secondary\" href=\"#\" role=\"button\">Edit</a> <a class=\"btn btn-primary btn-danger\" href=\"#\" role=\"button\">Delete</a>");
+				out.println("						</td>");
+				out.println("					</tr>");
 			}
-			out.println("<tr>");
-			out.println("	<td><input type=\"text\" class=\"form-control\" name=\"name\" placeholder=\"Name of Assignment\"></td>");
-			out.println("	<td><input type=\"text\" class=\"form-control\" name=\"score\" placeholder=\"Your Score\"></td>");
-			out.println("	<td><input type=\"text\" class=\"form-control\" name=\"worth\" placeholder=\"Worth\"></td>");
-			out.println("	<td><a class=\"btn btn-primary\" href=\"#\" role=\"button\">Add</a></td>");
-			out.println("</tr>");
-			out.println("	</table>");
+			out.println("					<tr>");
+			out.println("						<td><input type=\"text\" class=\"form-control\" name=\"name\" placeholder=\"Name of Assignment\"></td>");
+			out.println("						<td><input type=\"text\" class=\"form-control\" name=\"score\" placeholder=\"Your Score\"></td>");
+			out.println("						<td><input type=\"text\" class=\"form-control\" name=\"worth\" placeholder=\"Worth\"></td>");
+			out.println("						<td><a class=\"btn btn-primary\" href=\"#\" role=\"button\">Add</a></td>");
+			out.println("					</tr>");
+			out.println("				</table>");
 			
 		}
 
-		out.println("</div>");
+		out.println("		</div>");
 
-		out.println("</div>");
+		out.println("	</div>");
 		out.println("</body>");
 		out.println("</html>");
 	}
