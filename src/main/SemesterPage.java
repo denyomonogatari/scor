3f3a51a10b5fe205b908a3a097a1a77010dbafde
denyomonogatari <dvictor4@calstatela.edu>
2018-04-04 14:02:38 -0700

    SemesterPage refactor

--- src/main/SemesterPage.java
+++ src/main/SemesterPage.java
@@ -2,6 +2,7 @@ package main;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import models.Semester;
+import models.Course;
 import models.User;
 
 @WebServlet("/main/SemesterPage")
@@ -47,37 +48,38 @@ public class SemesterPage extends HttpServlet {
 		out.println("</head>");
 		out.println("<body>");
 		out.println("<div class=\"container\">");
-		out.println("<p class=\"text-right\"><a href=\"../auth/Logout\">Logout</a></p>");
-		out.println("<h1 class=\"display-2\">Hello, " + user.getFirst() + "</h1>");
-		out.println("<div class=\"row\">");
-		out.println("	<div class=\"col-6\">");
-		out.println("		<h2>Courses</h2>");
-		out.println("		<ul>");
-		int semesterId = Integer.parseInt(request.getParameter("semesterId"));
-		
-		for (int i = 0; i < user.getSemesters().get(semesterId).getCourses().size(); i++) 
-			out.println("			<li><a href=\"CoursePage?semesterId=" + semesterId + "&courseId=" + i + "\">" + user.getSemesters().get(semesterId).getCourses().get(i).getName() + " " + user.getSemesters().get(semesterId).getCourses().get(i).getGrade() + "</a></li>");
-		out.println("		</ul>");
-		
+		out.println("	<p class=\"text-right\"><a href=\"../auth/Logout\">Logout</a></p>");
+		out.println("	<h1 class=\"display-2\">Hello, " + user.getFirst() + "</h1>");
 		out.println("	<div class=\"row\">");
-		out.println("		<div class=\"col\">");
-		out.print("				<input type=\"text\" class=\"form-control\" name=\"semester\" placeholder=\"Name of Course\"> ");
+		out.println("		<div class=\"col-6\">");
+		out.println("			<h2>Courses</h2>");
+		out.println("			<ul>");
+		
+		int semesterId = Integer.parseInt(request.getParameter("semesterId"));
+		ArrayList<Course> courses = user.getSemesters().get(semesterId).getCourses();
+		for (int i = 0; i < courses.size(); i++) 
+			out.println("				<li><a href=\"CoursePage?semesterId=" + semesterId + "&courseId=" + i + "\">" + courses.get(i).getName() + " " + courses.get(i).getGrade() + "</a></li>");
+		out.println("			</ul>");
+		
+		out.println("		<div class=\"row\">");
+		out.println("			<div class=\"col\">");
+		out.println("					<input type=\"text\" class=\"form-control\" name=\"course\" placeholder=\"Name of Course\"> ");
+		out.println("			</div>");
+		out.println("			<div class=\"col\">");
+		out.println("					<input type=\"text\" maxlength=\"2\" class=\"form-control\" name=\"grade\" placeholder=\"Grade\"> ");
+		out.println("			</div>");
+		out.println("			<div class=\"col\">");
+		out.println("				<a class=\"btn btn-primary\" href=\"#\" role=\"button\">Add Course</a>");
+		out.println("			</div>");
+		out.println("		</div>");
+		out.println("		</div>");
+		out.println("		<div class=\"col-6\">");
+		out.println("			<h2>Dates</h2>");
+		out.println("			<p>All your upcoming events go here.</p>");
 		out.println("		</div>");
 		out.println("		<div class=\"col\">");
-		out.print("				<input type=\"text\" maxlength=\"2\" class=\"form-control\" name=\"grade\" placeholder=\"Grade\"> ");
-		out.println("		</div>");
-		out.println("		<div class=\"col\">");
-		out.println("			<a class=\"btn btn-primary\" href=\"#\" role=\"button\">Add Course</a>");
 		out.println("		</div>");
 		out.println("	</div>");
-		out.println("	</div>");
-		out.println("	<div class=\"col-6\">");
-		out.println("		<h2>Dates</h2>");
-		out.println("		<p>All your upcoming events go here.</p>");
-		out.println("	</div>");
-		out.println("	<div class=\"col\">");
-		out.println("	</div>");
-		out.println("</div>");
 
 		out.println("</div>");
 		out.println("</body>");
